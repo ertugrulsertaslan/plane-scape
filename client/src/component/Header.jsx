850cb25883f4878814ef19d45c93b9690741a8b4
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-09-21 15:38:51 +0300

    add mobile menu

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,6 +1,7 @@
 import { FaPlane } from "react-icons/fa";
 import { FaEarthAmericas } from "react-icons/fa6";
 import { IoTicket, IoReorderThreeOutline } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
@@ -10,7 +11,9 @@ const baseUrl = "http://localhost:3000"; // Base URL for API requests
 function Header() {
   const navigate = useNavigate();
   const [firstname, setFirstName] = useState();
+  const [userPhoto, setUserPhoto] = useState();
   const [userId, setUserId] = useState();
+  const [showMobilMenu, SetShowMobilMenu] = useState(false);
 
   // Function to refresh the authentication token
   const fetchToken = async () => {
@@ -40,6 +43,7 @@ function Header() {
         const response = await axios.get(`${baseUrl}/`, {
           withCredentials: true, // Send cookies with the request
         });
+        setUserPhoto(response.data.photoUrl);
         setFirstName(response.data.firstName);
         setUserId(response.data.userId);
       } catch (error) {
@@ -78,15 +82,57 @@ function Header() {
             onClick={() => navigate("/flights", { state: { userId } })}
             className="flex items-center space-x-2 cursor-pointer"
           >
-            <IoTicket className="text-purple-800" />
+            <div>
+              <img
+                src={userPhoto}
+                alt=""
+                className="w-10 h-10 object-cover rounded-full"
+              />
+            </div>
             <p>{firstname}</p>
           </div>
         </div>
         {/* Icon for mobile menu, shown only on small screens */}
         <div className="flex sm:hidden cursor-pointer">
-          <IoReorderThreeOutline size={40} />
+          <IoReorderThreeOutline
+            onClick={() => SetShowMobilMenu(!showMobilMenu)}
+            size={40}
+          />
         </div>
       </div>
+      {showMobilMenu && (
+        <div className="absolute w-10/12 h-1/2 bg-purple-500 top-20 rounded-lg shadow-lg p-4">
+          <div className="text-right">
+            <button
+              className="text-white font-bold"
+              onClick={() => SetShowMobilMenu(false)}
+            >
+              <IoMdClose size={25} />
+            </button>
+          </div>
+          <div className="flex flex-col items-center gap-4 mt-4">
+            <div className="flex items-center space-x-2 w-full border-t border-white justify-center py-5">
+              <IoTicket className="text-white text-2xl" />
+              <p className="text-white text-lg font-semibold">Details</p>
+            </div>
+            <div className="flex items-center space-x-2 w-full border-t border-white justify-center py-5">
+              <FaEarthAmericas className="text-white text-2xl" />
+              <p className="text-white text-lg font-semibold">Discover</p>
+            </div>
+            <div
+              onClick={() => navigate("/flights", { state: { userId } })}
+              className="flex items-center space-x-2 cursor-pointer w-full justify-center py-5 border-t border-white"
+            >
+              <img
+                src={userPhoto}
+                alt="User"
+                className="md:w-10 md:h-10 w-16 h-16 mr-5 md:m-0 rounded-full"
+              />
+              <p className="text-white text-lg font-semibold">{firstname}</p>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
